ff3d20b799cff27b3e60be5998f2d177d94bb1aa
Ayush <ayushbunkar100@gmail.com>
2026-03-31 14:39:07 +0530

    Fix api env loading

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@
  * Routes all /api/* requests to the Express app
  */
 
+// Load env vars first
+import dotenv from 'dotenv';
+dotenv.config({ path: '../server/.env' });
+
 import app from '../server/server.js';
 
 export default app;

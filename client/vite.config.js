f9e3c43a0298d5597a8e47ebf922cdf0f10ca7f2
Ayush <ayushbunkar100@gmail.com>
2026-03-31 11:10:24 +0530

    feat: add build configuration for chunk size and manual chunking in Vite setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from default 500kb to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'icons': ['react-icons'],
+          'chart': ['chart.js', 'react-chartjs-2'],
+        }
+      }
+    }
+  }
 })
